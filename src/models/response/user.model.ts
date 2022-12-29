33eeae883192a8dfe12e239289a04b992c608543
anhocva214 <anhocva214@gmail.com>
2022-12-29 14:24:18 +0700

    user: update info

--- src/models/response/user.model.ts
+++ src/models/response/user.model.ts
@@ -1,19 +1,31 @@
 export class User {
     id: string;
-    fullname: string;
+    fullName: string;
     email: string;
     gender: string;
     birthday: string;
     phoneNumber: string;
+    password: string;
 
     constructor();
     constructor(obj?: User);
     constructor(obj?: any){
         this.id = obj?.id || null;
-        this.fullname = obj?.fullname || null;
+        this.fullName = obj?.fullName || null;
         this.email = obj?.email || null;
-        this.gender = obj?.gender || null;
+        this.gender = obj?.gender || "other";
         this.birthday = obj?.birthday || null;
         this.phoneNumber = obj?.phoneNumber || null;
+        this.password = obj?.password || null;
+    }
+}
+
+export class UserUpdate extends User{
+    passwordComfirm: string;
+    constructor();
+    constructor(obj?: UserUpdate);
+    constructor(obj?: any){
+        super(obj);
+        this.passwordComfirm = obj?.passwordComfirm || null;
     }
 }
--- src/pages/cai-dat.tsx
+++ src/pages/cai-dat.tsx
@@ -1,8 +1,65 @@
+import { userApi } from "@apis/user.api";
 import MainLayout from "@layouts/main-layout";
+import { useAppDispatch } from "@redux/index";
+import { updateUser } from "@redux/user.redux";
 import { ROUTES } from "@utils/routes";
+import { DatePicker, message } from "antd";
+import { GetServerSideProps } from "next";
+import { ChangeEvent, useEffect, useState } from "react";
+import { User, UserUpdate } from "src/models/response/user.model";
+import dayjs from 'dayjs'
 
 
-export default function SettingPage() {
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+    try {
+        let id = req.cookies?.userId
+        let user = await userApi.getUserById(id)
+        return { props: { user } }
+    }
+    catch (err) {
+        // console.log(err)
+        return { props: {}, redirect: { destination: '/dang-nhap' } }
+    }
+}
+
+interface IProps {
+    user: User
+}
+
+export default function SettingPage({
+    user
+}: IProps) {
+    const dispatch = useAppDispatch()
+    const [form, setForm] = useState(new User(user))
+    const [confirmNewPassword, setConfirmNewPassword] = useState('')
+
+    useEffect(() => {
+        setConfirmNewPassword(user.password)
+    }, [])
+
+    const onChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>)=>{
+        let temp = {...form}
+        temp[e.target.name] = e.target.value
+        setForm(temp)
+    }
+
+    const onSave = () => {
+        if (confirmNewPassword != form.password) {
+            message.error("Mật khẩu không trùng khớp")
+            return;
+        }
+
+        dispatch(updateUser(new UserUpdate({ ...form, passwordComfirm: confirmNewPassword }))).unwrap()
+            .then(data => {
+                setForm({...user, ...data})
+                setConfirmNewPassword(confirmNewPassword)
+                message.success("Thành công")
+            })
+            .catch(error => {
+                console.log("🚀 ~ file: cai-dat.tsx:52 ~ onSave ~ error", error)
+                message.error("Thất bại")
+            })
+    }
 
     return (
         <MainLayout pageActive={ROUTES.setting}>
@@ -15,44 +72,53 @@ export default function SettingPage() {
                     </div>
                     <div className="change">
                         <h3>Thông tin cá nhân</h3>
-                        <a href="#">Chỉnh Sửa</a>
+                        <a onClick={onSave} role='button' >Lưu</a>
                     </div>
                     <form className="input-field">
                         <div className="first-item">
                             <div className="name">
                                 <p>Họ và tên</p>
-                                <input type="text" placeholder="Mafulzul Islam" readOnly />
+                                <input
+                                    type="text"
+                                    value={form?.fullName}
+                                    name="fullname"
+                                    onChange={onChange}
+                                    className="border outline-none"
+                                />
                             </div>
                             <div className="sex">
                                 <p>Giới tính</p>
-                                <select name="cars" id="cars">
-                                    <option value="nam" selected>Nam</option>
-                                    <option value="nu" disabled>Nữ</option>
+                                <select name="gender" value={form?.gender} onChange={onChange} className="border outline-none">
+                                    <option value="male" >Nam</option>
+                                    <option value="female" >Nữ</option>
+                                    <option value="other" >Chưa xác định</option>
                                 </select>
                             </div>
                         </div>
                         <div className="second-item">
                             <div className="birthday">
                                 <p>Ngày sinh nhật</p>
-                                <input type="date" placeholder="27/8/2998" readOnly />
+                                <DatePicker className="h-[50px]" style={{width: '100%'}} format={'DD/MM/YYYY'} value={dayjs(form?.birthday)} onChange={(value, dateString) => {
+                                    setForm({...form, birthday: value.toISOString()})
+                                }} />
                             </div>
                             <div className="phone">
                                 <p>Số điện thoại</p>
-                                <input type="text" placeholder={'+123456789'} readOnly />
+                                <input onChange={onChange} name="phoneNumber" type="text" value={form?.phoneNumber} className="border outline-none" />
                             </div>
                         </div>
                         <div className="third-item">
                             <p>Email</p>
-                            <input type="email" placeholder="abc@gmail.com" readOnly />
+                            <input onChange={onChange} name="email" type="email" value={form?.email} className="border outline-none" />
                         </div>
                         <div className="fourth-item">
                             <div className="new-pass">
                                 <p>Mật khẩu mới</p>
-                                <input type="password" placeholder="..." readOnly />
+                                <input onChange={onChange} name="password" type="password" value={form?.password} className="border outline-none" />
                             </div>
                             <div className="new-pass-confirm">
                                 <p>Nhập lại mật khẩu mới</p>
-                                <input type="password" placeholder="..." readOnly />
+                                <input onChange={e => setConfirmNewPassword(e.target.value)} type="password" value={confirmNewPassword} className="border outline-none" />
                             </div>
                         </div>
                     </form>
